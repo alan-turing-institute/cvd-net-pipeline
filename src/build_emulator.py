2c6bed3633b82a364dc2f56f50ab1211b03f6bd6
FayFrost107 <145458276+FayFrost107@users.noreply.github.com>
2025-05-15 11:18:19 +0100

    upgrade toml

--- src/build_emulator.py
+++ src/build_emulator.py
@@ -1,3 +1,22 @@
+from ModularCirc.Models.KorakianitisMixedModel import KorakianitisMixedModel, KorakianitisMixedModel_parameters, TEMPLATE_TIME_SETUP_DICT
+import numpy as np
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import KFold
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import  PowerTransformer
+import contextlib
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+
+
 # steps/build_emulator.py
 def build_emulator():
     print("[BuildEmulator] Running PCA and training emulator (placeholder)")
