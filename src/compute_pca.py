35b695f5c55879495376b30a503df5362ef86dcf
Levan Bokeria <levan.bokeria@gmail.com>
2025-05-21 11:51:44 +0100

    changing the naming of saved files

--- src/compute_pca.py
+++ src/compute_pca.py
@@ -9,7 +9,9 @@ from utils import plot_utils
 def compute_pca(n_samples:int=500, n_params:int=5, n_pca_components:int=10, output_path:str="output"):
     print("[ComputePCA] Running PCA (placeholder)")
 
-    output_file = pd.read_csv(f"{output_path}/output_{n_samples}_{n_params}params/waveform_resampled_all_pressure_traces_rv.csv")
+    output_file_name = 'waveform_resampled_all_pressure_traces_rv.csv'
+
+    output_file = pd.read_csv(f"{output_path}/output_{n_samples}_{n_params}params/{output_file_name}")
 
     # Create direcoty for results
     if not os.path.exists(f"{output_path}/output_{n_samples}_{n_params}params/pca"):
@@ -42,7 +44,11 @@ def compute_pca(n_samples:int=500, n_params:int=5, n_pca_components:int=10, outp
 
     # Concatenate the PCA components with the original data     
     df_pca = pd.concat([df, X_pca], axis=1)
-    df_pca.to_csv(f'{output_path}/output_{n_samples}_{n_params}params/resampled_all_pressure_traces_rv_with_pca.csv', index=False)
+
+    # Create a new name for the output file which appends "_with_pca" to the original name
+    output_file_name_pca = output_file_name.replace('.csv', '_with_pca.csv')
+
+    df_pca.to_csv(f'{output_path}/output_{n_samples}_{n_params}params/{output_file_name_pca}', index=False)
 
     output_parameters = os.path.join(output_path, f'output_{n_samples}_{n_params}params')
 
