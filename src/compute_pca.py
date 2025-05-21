70736489452ed989a780a280d17d730bb8fdea5c
Levan Bokeria <levan.bokeria@gmail.com>
2025-05-21 11:31:24 +0100

    cleaning up imports

--- src/compute_pca.py
+++ src/compute_pca.py
@@ -1,21 +1,9 @@
-from ModularCirc.Models.KorakianitisMixedModel import KorakianitisMixedModel, KorakianitisMixedModel_parameters, TEMPLATE_TIME_SETUP_DICT
-import numpy as np
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
-import seaborn as sns
-import sklearn
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import KFold
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import  PowerTransformer
-import contextlib
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import r2_score
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split
-from utils import utils, plot_utils
+from utils import plot_utils
 
 
 # steps/compute.py
