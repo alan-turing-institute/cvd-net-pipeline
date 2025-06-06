e1a0326f207381b4441570e85b333d8c9eb87511
Levan Bokeria <levan.bokeria@gmail.com>
2025-06-06 15:08:50 +0100

    increasing number of parallel jobs when simulating

--- src/simulate_data.py
+++ src/simulate_data.py
@@ -78,7 +78,7 @@ def simulate_data(param_path: str, n_samples: int, output_path: str, repeat_simu
     if repeat_simulations:
         os.makedirs(output_parameters_simulations, exist_ok=True)
         simulations = br.run_batch(
-            n_jobs=7,
+            n_jobs=10,
             output_path=output_parameters_simulations
         )
 
