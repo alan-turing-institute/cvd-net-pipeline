027981934f05dda6c5084a91f94bbd8b98c29b89
Levan Bokeria <levan.bokeria@gmail.com>
2025-06-19 17:20:15 +0100

    changing directory names for test_compute_pca.py

--- tests/test_compute_pca.py
+++ tests/test_compute_pca.py
@@ -7,9 +7,9 @@ import shutil
 
 @pytest.fixture
 def cleanup_output():
-    output_file = "tests/inputs_for_tests/compute_pca_module/output_50_9params/waveform_resampled_all_pressure_traces_rv_with_pca.csv"
-    output_pca_folder = "tests/inputs_for_tests/compute_pca_module/output_50_9params/pca"
-    output_figures_folder = "tests/inputs_for_tests/compute_pca_module/output_50_9params/figures"
+    output_file = "tests/inputs_for_tests/compute_pca_module/output_50_9_params/waveform_resampled_all_pressure_traces_rv_with_pca.csv"
+    output_pca_folder = "tests/inputs_for_tests/compute_pca_module/output_50_9_params/pca"
+    output_figures_folder = "tests/inputs_for_tests/compute_pca_module/output_50_9_params/figures"
     yield output_file
     if os.path.exists(output_file):
         os.remove(output_file)
@@ -28,5 +28,5 @@ def test_compute_pca(cleanup_output):
 
     # Check if the output data matches the expected output
     output_data = pd.read_csv(cleanup_output)
-    expected_output = pd.read_csv('tests/expected_outputs/compute_pca_module/output_50_9params/waveform_resampled_all_pressure_traces_rv_with_pca.csv')
+    expected_output = pd.read_csv('tests/expected_outputs/compute_pca_module/output_50_9_params/waveform_resampled_all_pressure_traces_rv_with_pca.csv')
     pd.testing.assert_frame_equal(output_data, expected_output)
