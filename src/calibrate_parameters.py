22ce62478782dc2807293f621a692245d06f3f60
Levan Bokeria <levan.bokeria@gmail.com>
2025-06-27 10:34:54 +0100

    combined real and synthetic calibration scripts, excluding plotting bits

--- src/calibrate_parameters.py
+++ src/calibrate_parameters.py
@@ -11,6 +11,7 @@ def calibrate_parameters(data_type="synthetic",
                          n_samples:int=50, 
                          n_params:int=9, 
                          output_path:str='output', 
+                         emulator_path:str=None,
                          output_keys:list=None,
                          include_timeseries:bool=True,
                          epsilon_obs_scale:float=0.05,
@@ -19,16 +20,30 @@ def calibrate_parameters(data_type="synthetic",
 
     file_suffix = f'_{n_samples}_{n_params}_params'
 
-    # Data
-    input_params = pd.read_csv(f'{output_path}/input{file_suffix}.csv')
-    output_file = pd.read_csv(f"{output_path}/output{file_suffix}/waveform_resampled_all_pressure_traces_rv_with_pca.csv")
+    if data_type == "synthetic":
 
-    # emulators
-    emulators = pd.read_pickle(f"{output_path}/output{file_suffix}/emulators/linear_models_and_r2_scores_{n_samples}.pkl")
-    
+        dir_name = f"{output_path}/output{file_suffix}"
+
+        input_params = pd.read_csv(f'{output_path}/input{file_suffix}.csv')
+
+        # emulators
+        emulators = pd.read_pickle(f"{dir_name}/emulators/linear_models_and_r2_scores_{n_samples}.pkl")
+        
+    elif data_type == "real":
+
+        dir_name = output_path
+
+        input_params = pd.read_csv(f'{emulator_path}/input_{n_samples}_{n_params}_params.csv')
+
+        # emulators
+        emulators = pd.read_pickle(f"{emulator_path}/output_{n_samples}_{n_params}_params/emulators/linear_models_and_r2_scores_{n_samples}.pkl")
+        print(f"Using trained emulators from: {emulator_path}/output_{n_samples}_{n_params}_params.")
+
+    # Data
+    output_file = pd.read_csv(f"{dir_name}/waveform_resampled_all_pressure_traces_rv_with_pca.csv")
 
     # Direcotry for saving results
-    output_dir = f"{output_path}/output{file_suffix}/bayesian_calibration_results/"
+    output_dir = f"{dir_name}/bayesian_calibration_results/"
 
     # Make directory if it doesn't exist
     if not os.path.exists(output_dir):
@@ -52,26 +67,48 @@ def calibrate_parameters(data_type="synthetic",
     # Select emulators and data for specified output_keys
     emulator_output = emulators.loc[all_output_keys]
     observation_data = output_file.loc[:, all_output_keys]
+
+    if data_type == "synthetic":
+
+        # Create the diagonal matrix
+        e_obs = np.diag(diagonal_values) * epsilon_obs_scale
         
-    # Create the diagonal matrix
-    e_obs = np.diag(diagonal_values) * epsilon_obs_scale
-    
-    bc = BayesianCalibration(input_prior=input_params, 
-                             emulator_output=emulator_output, 
-                             observation_data=observation_data, 
-                             which_obs=3, 
-                             epsilon_obs = e_obs,
-                             data_type=data_type)
+        bc = BayesianCalibration(input_prior=input_params, 
+                                emulator_output=emulator_output, 
+                                filtered_output=observation_data, 
+                                which_obs=3, 
+                                epsilon_obs = e_obs,
+                                data_type=data_type)
 
-    bc.compute_posterior()
+        bc.compute_posterior()
 
-    # Save the posterior mean and covariance
-    posterior_mean = pd.DataFrame(bc.Mu_post, index=bc.param_names, columns=['Posterior Mean'])    
-    posterior_cov = pd.DataFrame(bc.Sigma_post, index=bc.param_names, columns=bc.param_names)
+        # Save the posterior mean and covariance
+        posterior_mean = pd.DataFrame(bc.Mu_post, index=bc.param_names, columns=['Posterior Mean'])    
+        posterior_cov = pd.DataFrame(bc.Sigma_post, index=bc.param_names, columns=bc.param_names)
 
-    
-    # Smaple from the posterior distribution
-    bc.sample_posterior(n_samples=n_samples)
+        
+        # Smaple from the posterior distribution
+        bc.sample_posterior(n_samples=n_samples)
+
+    elif data_type == "real":
+        
+        posterior_means = []
+
+        # Create the diagonal matrix
+        e_obs = np.diag(diagonal_values) * epsilon_obs_scale
+
+        for row in range(len(observation_data)):
+            bc = BayesianCalibration(input_params, emulator_output, observation_data.iloc[row:row+1], epsilon_obs = e_obs)
+            bc.compute_posterior()
+            posterior_means.append(bc.Mu_post.squeeze())
+
+        # Convert the list to a NumPy array
+        posterior_means = np.array(posterior_means)
+        Sigma_post = bc.Sigma_post
+
+        # Save the posterior mean and covariance
+        posterior_mean = pd.DataFrame(posterior_means, columns=bc.param_names)    
+        posterior_cov = pd.DataFrame(Sigma_post, index=bc.param_names, columns=bc.param_names)
 
     n_output_keys =  len(all_output_keys)
 
@@ -83,9 +120,12 @@ def calibrate_parameters(data_type="synthetic",
     if not os.path.exists(output_dir_bayesian):
         os.makedirs(output_dir_bayesian)
 
-    bc.samples_df.to_csv(f"{output_dir_bayesian}/posterior_samples.csv", index=False)
     posterior_mean.to_csv(f"{output_dir_bayesian}/posterior_mean.csv", index=False)
     posterior_cov.to_csv(f"{output_dir_bayesian}/posterior_covariance.csv", index=False)
+
+    if data_type == "synthetic":
+        bc.samples_df.to_csv(f"{output_dir_bayesian}/posterior_samples.csv", index=False)
+
 
     # Save the config file
     with open(os.path.join(output_dir_bayesian, 'used_config.json'), 'w') as f:
