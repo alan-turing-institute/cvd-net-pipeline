70b3f5de7c907978c99b32f6bf23cba6db21b9b6
FayFrost107 <145458276+FayFrost107@users.noreply.github.com>
2025-07-01 16:40:09 +0100

    change pat.c sample range to 0.5-1.5

--- src/simulate_data.py
+++ src/simulate_data.py
@@ -80,7 +80,7 @@ def simulate_data(param_path: str, n_samples: int, output_path: str, repeat_simu
     if repeat_simulations:
         os.makedirs(output_parameters_simulations, exist_ok=True)
         simulations = br.run_batch(
-            n_jobs=10,
+            n_jobs=7,
             output_path=output_parameters_simulations
         )
 
