082a7291f992deae6ee648afbc51a6535b45e14a
Levan Bokeria <levan.bokeria@gmail.com>
2025-07-03 11:56:07 +0100

    some cosmetic change and removing ddof arg in bayesian calibration

--- src/simulate_data.py
+++ src/simulate_data.py
@@ -54,8 +54,16 @@ def simulate_data(param_path: str, n_samples: int, output_path: str, repeat_simu
     input_header = ','.join(br.samples.columns)
     file_suffix  = f'_{n_samples}_{n_params}_params'
 
-    np.savetxt(os.path.join(output_path,f'pure_input{file_suffix}.csv'), _pure_samples, header=','.join(_pure_samples.columns), delimiter=',')
-    np.savetxt(os.path.join(output_path,f'input{file_suffix}.csv'), br.samples, header=input_header, delimiter=',')
+    np.savetxt(os.path.join(output_path,
+                            f'pure_input{file_suffix}.csv'),
+                            _pure_samples,
+                            header=','.join(_pure_samples.columns),
+                            delimiter=',')
+    np.savetxt(os.path.join(output_path,
+                            f'input{file_suffix}.csv'),
+                            br.samples,
+                            header=input_header,
+                            delimiter=',')
 
     if sample_parameters:
         output_parameters = os.path.join(output_path, f'output{file_suffix}')
