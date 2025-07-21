3f1368df620fe9dbfec5d25c6020c61a3b0f33d8
Levan Bokeria <levan.bokeria@gmail.com>
2025-07-21 11:41:11 +0100

    moving analyse_giessen expected outputs to synthetic data subfolder

--- tests/test_analyse_giessen.py
+++ tests/test_analyse_giessen.py
@@ -4,6 +4,7 @@ import os
 import numpy as np
 from analyse_giessen import analyse_giessen
 
+
 @pytest.fixture
 def cleanup_output_file():
     output_file = "tests/inputs_for_tests/analyse_giessen_module/output_64_9_params/waveform_resampled_all_pressure_traces_rv.csv"
@@ -11,12 +12,14 @@ def cleanup_output_file():
     if os.path.exists(output_file):
         os.remove(output_file)
 
+
 @pytest.fixture
 def cleanup_calibration_output_file():
     output_file = "tests/inputs_for_tests/analyse_giessen_module/output_64_9_params/bayesian_calibration_results/17_output_keys/calibration_20250604_154542/waveform_resampled_all_pressure_traces_rv.csv"
     yield output_file
     if os.path.exists(output_file):
         os.remove(output_file)        
+
 
 def test_analyse_giessen_valid_input(cleanup_output_file):
 
@@ -27,8 +30,9 @@ def test_analyse_giessen_valid_input(cleanup_output_file):
 
     # Check if the output data matches the expected output
     output_data = pd.read_csv(cleanup_output_file)
-    expected_output = pd.read_csv('tests/expected_outputs/analyse_giessen_module/output_64_9_params/waveform_resampled_all_pressure_traces_rv.csv')
+    expected_output = pd.read_csv('tests/expected_outputs/analyse_giessen_module/output_64_9_params/synthetic_data/waveform_resampled_all_pressure_traces_rv.csv')
     pd.testing.assert_frame_equal(output_data[expected_output.columns], expected_output)
+
 
 def test_analyse_giessen_invalid_input():
     # Test with an invalid file path
@@ -36,6 +40,7 @@ def test_analyse_giessen_invalid_input():
         analyse_giessen(file_path="invalid/path",
                         data_type="synthetic",
                         gaussian_sigmas=[6., 4., 2.])
+
 
 def test_analyse_giessen_valid_calibrated_input(cleanup_calibration_output_file):
 
@@ -46,5 +51,5 @@ def test_analyse_giessen_valid_calibrated_input(cleanup_calibration_output_file)
 
     # Check if the output data matches the expected output
     output_data = pd.read_csv(cleanup_calibration_output_file)
-    expected_output = pd.read_csv('tests/expected_outputs/analyse_giessen_module/output_64_9_params/bayesian_calibration_results/17_output_keys/calibration_20250604_154542/waveform_resampled_all_pressure_traces_rv.csv')
+    expected_output = pd.read_csv('tests/expected_outputs/analyse_giessen_module/output_64_9_params/synthetic_data/bayesian_calibration_results/17_output_keys/calibration_20250604_154542/waveform_resampled_all_pressure_traces_rv.csv')
     pd.testing.assert_frame_equal(output_data[expected_output.columns], expected_output)
