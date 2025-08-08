579eb75c886a10c7a535b912a8b22cf76e5b57ac
Levan Bokeria <levan.bokeria@gmail.com>
2025-08-08 11:49:50 +0100

    moving all parameter jsons in a folder

--- sensitivity_analysis_pipeline.py
+++ sensitivity_analysis_pipeline.py
@@ -41,7 +41,7 @@ def run_pipeline(config):
             print("Warning: output_dir_sims is pre-defined in the configuration file. It will be overwritten by the value from the simulation step.")
 
         output_dir_sims, n_params = simulate_data(
-            param_path=config.get("input_parameters"),
+            param_path=os.path.join('./input_parameters_jsons', config.get("input_parameters")),
             n_samples=n_samples,
             output_path=output_path,
             sample_parameters=True
