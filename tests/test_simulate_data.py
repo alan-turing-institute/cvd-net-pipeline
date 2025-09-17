4bb3f6df96a0a1fcef1c054286c0c757afd33969
Levan Bokeria <levan.bokeria@gmail.com>
2025-09-17 14:00:51 +0100

    deleting the redundant cleanup code

--- tests/test_simulate_data.py
+++ tests/test_simulate_data.py
@@ -1,5 +1,4 @@
 import os
-import pytest
 import pandas as pd
 from cvdnet_pipeline.simulate_data import simulate_data
 import tempfile
@@ -7,15 +6,21 @@ import shutil
 
 RTOL_TOLERANCE = 1e-2
 
+from test_constants import (
+    DEFAULT_N_SAMPLES,
+    DEFAULT_N_PARAMS,
+    DEFAULT_EPSILON_OBS_SCALE
+)
+
 def test_simulate_data():
     # Define test parameters
 
     with tempfile.TemporaryDirectory() as tmp_path:
         print(f"Temporary directory created at: {tmp_path}")
 
-        param_path = os.path.join('./tests/inputs_for_tests/simulate_data_module/', 
+        param_path = os.path.join('./input_parameters_jsons_for_tests', 
                                   "parameters_pulmonary_sensitive_summarystats.json")  # Ensure this file exists with valid parameters
-        n_samples = 64
+        n_samples = DEFAULT_N_SAMPLES
         repeat_simulations = True
 
         # Call the function
@@ -27,17 +32,14 @@ def test_simulate_data():
         )
 
         # Verify that the output files are created
-        input_file = os.path.join(tmp_path,f'input_{n_samples}_9_params.csv')
-        output_dir_sims = os.path.join(tmp_path,f'output_{n_samples}_9_params')
-        bool_indices_file = os.path.join(output_dir_sims,f'bool_indices_{n_samples}.csv')
+        input_file                     = os.path.join(tmp_path,f'input_{n_samples}_{DEFAULT_N_PARAMS}_params.csv')
+        output_dir_sims                = os.path.join(tmp_path,f'output_{n_samples}_{DEFAULT_N_PARAMS}_params')
+        bool_indices_file              = os.path.join(output_dir_sims,f'bool_indices_{n_samples}.csv')
         output_dir_pressure_traces_pat = os.path.join(output_dir_sims,'pressure_traces_pat','all_pressure_traces.csv')
-        output_dir_pressure_traces_rv = os.path.join(output_dir_sims,'pressure_traces_rv','all_pressure_traces.csv')
-
-
+        output_dir_pressure_traces_rv  = os.path.join(output_dir_sims,'pressure_traces_rv','all_pressure_traces.csv')
         assert os.path.exists(input_file), "Input file was not created."
         assert os.path.exists(output_dir_sims), "Simulations Output directory was not created."
         assert os.path.exists(bool_indices_file), "Bool indices file was not created."
-
 
         # Optionally, check the contents of the input file
         input_data = pd.read_csv(input_file)
@@ -45,14 +47,14 @@ def test_simulate_data():
 
         # Compare the input file to the input file in the expected_outputs directory
         expected_input_file_path = os.path.join('./tests/expected_outputs/simulate_data_module',
-                                           f'output_{n_samples}_9_params/',
-                                           f'input_{n_samples}_9_params.csv')
+                                           f'output_{n_samples}_{DEFAULT_N_PARAMS}_params/',
+                                           f'input_{n_samples}_{DEFAULT_N_PARAMS}_params.csv')
         expected_input_data = pd.read_csv(expected_input_file_path)
         pd.testing.assert_frame_equal(input_data, expected_input_data)
 
         # Compare the output files to the expected output files
         expected_output_dir = os.path.join('./tests/expected_outputs/simulate_data_module',
-                                            f'output_{n_samples}_9_params/')
+                                            f'output_{n_samples}_{DEFAULT_N_PARAMS}_params/')
         expected_pressure_traces_pat = pd.read_csv(os.path.join(expected_output_dir,
                                                     'pressure_traces_pat',
                                                     'all_pressure_traces.csv'))
@@ -94,7 +96,7 @@ def test_simulate_data():
 
         # Run the test for calibrated parameters
         output_dir_bayesian = os.path.join('./tests/expected_outputs/calibrate_parameters_module',
-                                           'synthetic_data/output_64_9_params/bayesian_calibration_results',
+                                           'synthetic_data/output_64_{DEFAULT_N_PARAMS}_params/bayesian_calibration_results',
                                            '17_output_keys/calibration_20250827_151335')
 
         output_dir_sims, n_params = simulate_data(
@@ -109,7 +111,7 @@ def test_simulate_data():
 
         # Compare the output files to the expected output files
         expected_output_dir = os.path.join('./tests/expected_outputs/simulate_data_module',
-                                            f'output_{n_samples}_9_params/',
+                                            f'output_{n_samples}_{DEFAULT_N_PARAMS}_params/',
                                             'bayesian_calibration_results/17_output_keys/calibration_20250827_151335/')
         expected_pressure_traces_pat = pd.read_csv(os.path.join(expected_output_dir,
                                                     'pressure_traces_pat',
@@ -129,10 +131,3 @@ def test_simulate_data():
                                       expected_pressure_traces_rv,
                                       check_exact=False,
                                       rtol=RTOL_TOLERANCE)
-
-        # Delete the output directory to clean up
-        shutil.rmtree(os.path.join(output_dir_bayesian,'figures'))
-        shutil.rmtree(os.path.join(output_dir_bayesian,'pressure_traces_pat'))
-        shutil.rmtree(os.path.join(output_dir_bayesian,'pressure_traces_rv'))
-        shutil.rmtree(os.path.join(output_dir_bayesian,'posterior_simulations'))        
-        os.remove(os.path.join(output_dir_bayesian, 'bool_indices_64.csv'))
